fa798919e771aa950320ce35c3c77802004ba94f
unknown <thomas.lw@puccampinas.edu.br>
2026-04-08 18:36:20 -0300

    Alteração no codigo de erro

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 def number_option_input(opcao_min, opcao_max):
-    """
+     """
     Validar input do usuário entre dois valores, diante de um menu numérico
 
     Args:
@@ -9,13 +9,18 @@ def number_option_input(opcao_min, opcao_max):
     Returns:
         int: opção válida escolhida pelo usuário
     """
-    opcao=-1
-    while(opcao<opcao_min or opcao>opcao_max):
+     opcao = -1
+     while (opcao < opcao_min or opcao > opcao_max):
         try:
-            opcao=int(input("\nDigite sua opção: "))
+            
+            opcao = int(input("\nDigite sua opção: "))
         except:
+            
             print("Erro! Digite um valor numérico")
-        if opcao>=opcao_min and opcao<=opcao_max:
-            return opcao
         else:
-            print(f"Erro! Digite um valor entre {opcao_min} e {opcao_max}")
+            
+            if opcao >= opcao_min and opcao <= opcao_max:
+                return opcao
+            else:
+                
+                print(f"Erro! Digite um valor entre {opcao_min} e {opcao_max}")
