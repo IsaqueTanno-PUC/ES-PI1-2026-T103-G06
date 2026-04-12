0e2af40aeaceac3a3e62c1914788d57b57fe48e4
Fabrício Santos <fabricio.ss1@puccampinas.edu.br>
2026-04-12 16:55:22 -0300

    Banco de Dados

---
+++ BDsistema_votacao.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE sistema_votacao;
+
+USE sistema_votacao;
+
+CREATE TABLE eleitores (
+	eleitor_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    eleitor_nome VARCHAR (50) NOT NULL,
+    eleitor_titulo VARCHAR (12) NOT NULL,
+    eleitor_cpf VARCHAR (11) UNIQUE NOT NULL,
+    eleitor_mesario BOOL DEFAULT FALSE NOT NULL,
+    eleitor_horavoto DATETIME,
+    eleitor_chaveacesso CHAR (7)
+);
+
+CREATE TABLE candidatos (
+	cand_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    can_nome VARCHAR(50),
+    cand_numero INT NOT NULL,
+    cand_partido VARCHAR (20)
+);
+
+CREATE TABLE votos (
+	voto_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    voto_horario DATETIME,
+    voto_candnumero INT,
+    voto_protocolo INT
+);
